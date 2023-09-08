64426866ed03c38b0c502e3b72181822f2353678
George Michael Pussl <george.pussl@gmail.com>
2023-09-08 07:28:31 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,3 @@
-/*
- * File: 100-print_comb3.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
@@ -13,24 +8,24 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+for (digit1 = 0; digit1 < 9; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+{
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
-			if (digit1 == 8 && digit2 == 9)
-				continue;
+if (digit1 == 8 && digit2 == 9)
+continue;
 			
-			putchar(',');
-			putchar(' ');
+putchar(',');
+putchar(' ');
 		}
-	}
+}
 
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
