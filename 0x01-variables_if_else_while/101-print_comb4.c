c7163d80f1ed67f16f1e0994841d81cfd2815cc4
George Michael Pussl <george.pussl@gmail.com>
2023-09-08 09:26:12 -0700

    Update Optional

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,9 +19,8 @@ for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 putchar((digit3 % 10) + '0');
-
 if  (digit1 == 7 && digit2 == 8 && digit3 == 9)
-continue;				
+continue;
 putchar(',');
 putchar(' ');
 }
