ae93ebf4347010e8ffc7ac23ccf6db4061a55655
George Michael Pussl <george.pussl@gmail.com>
2023-09-20 12:51:34 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; dest[i] != '\0'; i++)
-                ;
-        for (j = 0; src[j] != '\0'; j++)
-        {
-                dest[i] = src[j];
-                i++;
-        }
-        return (dest);
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
