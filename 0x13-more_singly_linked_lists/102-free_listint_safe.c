9f7d581f648094e31b2d2ddb3393a28a2b9c9171
George Michael Pussl <george.pussl@gmail.com>
2023-10-24 15:00:06 -0700

    Free safe version

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,43 +1,33 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
-* free_listint_safe - Frees a listint_t linked list.
-* @h: A pointer to a pointer to the head of the list.
+* free_listint_safe - Frees a listint_t list safely.
+* @h: A pointer to a pointer to the first node of the list.
 * Return: The size of the list that was freed.
 */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count_new = 0, count_comp = 0;
-	listint_t *tmp, *head, *comp;
+	size_t count = 0;
+	listint_t *current = *h;
+	listint_t *next;
 
-	if (h == NULL || *h == NULL)
-	return (0);
+	while (current != NULL)
+	{
+	count++;
 
-	head = comp = tmp = *h;
-	count_new = 0;
-
-	while (head != NULL)
-{
-	comp = *h;
-	count_comp = 0;
-	while (count_new > count_comp)
-{
-	if (tmp == comp)
-{
+	if (current < current->next)
+	{
+	next = current->next;
+	free(current);
+	current = next;
+	}
+	else
+	{
+	free(current);
 	*h = NULL;
-	return (count_new);
-}
-	count_comp++;
-	comp = comp->next;
-}
-	count_new++;
-	tmp = head->next;
-	free((void *)head);
-	head = tmp;
+	break;
+	}
 }
 
-*h = tmp;
-	return (count_new);
+return (count);
 }
-
